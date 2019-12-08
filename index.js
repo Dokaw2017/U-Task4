933b97c09ec25eabae4af3e3f9ecc15bb2009541
nebi <snoopafr@gmail.com>
2019-12-08 21:02:11 +0200

    Task four started and finished

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict'
+const berry = document.getElementById('berry');
+console.log(berry.outerHTML);
+console.log('I found the berry: ', berry.innerHTML);
+berry.style.backgroundColor = 'red';
+
+const orange = document.querySelector('[data-foodtype="squishy"]');
+console.log(orange.outerHTML);
+console.log('I found the fruit: ', orange.textContent);
+orange.style.backgroundColor = 'orange';
+
+const liCollection = document.getElementsByTagName('li');
+console.log(liCollection);
+console.log('Using the for loop here: ' );
+for (var i = 0; i<liCollection.length; i++){
+    console.log( liCollection[i].innerHTML );    
+    liCollection[i].innerHTML == 'Pear'? liCollection[i].style.backgroundColor = 'green': false;
+    liCollection[i].style.width = '100px';
+    liCollection[i].style.listStyleType = 'none';    
+}    
+
+
+const liCollection2 = document.querySelectorAll('li');
+console.log(liCollection2);
+console.log('Using the forEach here: ');
+liCollection2.forEach(li => {
+    li.style.border = '1px solid black'
+    console.log(li.textContent);
+});
